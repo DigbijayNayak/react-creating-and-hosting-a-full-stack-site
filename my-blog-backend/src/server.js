67a44067b22316919f0d7b8002d6af5742f9bf43
DigbijayNayak <digbijaynayak522@gmail.com>
2023-04-30 17:34:46 +0530

    Upvoting articles

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,20 @@
 import express from "express";
 
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+  },
+];
+
 const app = express();
 app.use(express.json());
 
@@ -11,6 +26,17 @@ app.post("/hello", (req, res) => {
 app.get("/hello/:name", (req, res) => {
   const { name } = req.params;
   res.send(`Hello ${name}!!`);
+});
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name === name);
+  if (article) {
+    article.upvotes += 1;
+    res.send(`The ${name} article now has ${article.upvotes} upvotes.`);
+  } else {
+    res.send("The article doesn't exist");
+  }
 });
 
 app.listen(8000, () => {
