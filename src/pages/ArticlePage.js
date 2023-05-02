85a183e69b585a9127e1b2d5cd997cbf08277a35
DigbijayNayak <digbijaynayak522@gmail.com>
2023-05-02 12:37:34 +0530

    Adding React hooks

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
 const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+
+  useEffect(() => {
+    setArticleInfo({upvotes: 3, comments: []});
+  }, []);
+
   const { articleId } = useParams();
   const article = articles.find((article) => article.name === articleId);
+
   if(!article){
     return <NotFoundPage />
   }
+
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This article has {articleInfo.upvotes} upvote(s).</p>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
