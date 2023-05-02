cb1f0009d25f17683330c9b88e9c621d47078345
DigbijayNayak <digbijaynayak522@gmail.com>
2023-05-02 08:27:41 +0530

    Adding MongoDB to Express

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { MongoClient } from "mongodb";
 let articlesInfo = [
   {
     name: "learn-react",
@@ -31,12 +31,27 @@ app.get("/hello/:name", (req, res) => {
   res.send(`Hello ${name}!!`);
 });
 
+app.get("/api/articles/:name", async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient("mongodb://127.0.0.1:27017");
+  await client.connect();
+
+  const db = client.db("react-blog-db"); // use react-blog-db;
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    res.json(article);
+  } else {
+    res.sendStatus(404).send("Articles not found!");
+  }
+});
 app.put("/api/articles/:name/upvote", (req, res) => {
   const { name } = req.params;
   const article = articlesInfo.find((a) => a.name === name);
   if (article) {
     article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes.`);
+    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
   } else {
     res.send("The article doesn't exist");
   }
